a4da969bcf2f07b11aceb4717ae6bca8e205f792
Raheela Shaheen <raheelashaheen31@gmail.com>
2025-06-30 17:56:28 -0700

    pic move

--- src/screens/Listing.js
+++ src/screens/Listing.js
@@ -2,16 +2,17 @@ import React from "react";
 import { ScrollView, View, Image } from "react-native";
 
 export default function Listing() {
+  const data= [1, 2, 3, 4,5];
   return (
-    <ScrollView horizontal={true} showsHorizontalScrollIndicator={true}>
-      <View style={{ flexDirection: "row" }}>
-        {[1, 2, 3, 4].map((item, index) => (
+    <ScrollView horizontal={true} showHorizontalllIndicator={true}>
+      <View style={{ flexDirection: "column" }}>
+        {data.map((item, index) => (
           <Image
             key={index}
             style={{
-              width: 300,
-              height: 300,
-              marginHorizontal: 10,
+              width: 200,
+              height: 200,
+              marginVerticals: 10,
             }}
             source={require('../../assets/images.png')} // ← correct
           />
